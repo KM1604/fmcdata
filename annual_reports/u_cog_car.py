ffcc8e0f5c2b31acbd6108a3a91580fbac3aa667
Kevin Eccles <kevin.eccles@fmcusa.org>
2026-02-11 07:35:39 -0500

    pipeline updated for 2025 annual report submissions. Synced to cognito and refreshes annual report reminders in SharePoint

--- annual_reports/u_cog_car.py
+++ annual_reports/u_cog_car.py
@@ -2,8 +2,9 @@ import json
 import polars as pl
 import pyodbc
 import requests
+import time
 import tomllib
-
+import win32com.client
 
 class SQLServer:
     def __init__(self, cfg_dict):
@@ -67,6 +68,24 @@ def read_toml(path):
     return cfg
 
 
+def refresh_excel(path):
+    xlapp = win32com.client.DispatchEx("Excel.Application")
+    try:
+        print(f'''opening {path}''')
+        Workbook = xlapp.Workbooks.Open(path)
+        print('Waiting 5 seconds because MS is slow')
+        time.sleep(5)
+        print('refreshing data connections')
+        Workbook.RefreshAll()
+        xlapp.CalculateUntilAsyncQueriesDone()
+        Workbook.Close(SaveChanges=1)
+        xlapp.Quit()
+    except:
+        print('refresh failed')
+    else:
+        print('refresh complete, save and quit')
+
+
 def return_query(conn_string, query):
     df = pl.read_database_uri(query, conn_string)
     print(df)
@@ -107,7 +126,8 @@ def main():
     writecsv_from_frame(car, "car_bk.csv")
     execute_sql(FMCSQL.pyodbc_conn, "EXECUTE u_cog_car")
     execute_sql(FMCSQL.pyodbc_conn, "DROP TABLE s_cog_car")
-
+    execute_sql(FMCSQL.pyodbc_conn, "EXECUTE u_domo_church_annual_reports")
+    refresh_excel('C:\\Users\\kevin.eccles\\Free Methodist Church USA\\WMC Team - Documents\\ADM - Administration Files\\Database Administration\\Source Files\\Annual Reports\\annual_report_reminders.xlsx')
 
 if __name__ == "__main__":
     main()
